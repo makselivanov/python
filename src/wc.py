74336fdd6beca346f21470252f7bcdc279c557ae
Makar Selivanov <makar.selivanov@gmail.com>
2024-02-27 15:42:27 +0300

    fix wc, but word count is wrong

--- src/wc.py
+++ src/wc.py
@@ -11,13 +11,13 @@ def wc(file):
         if not buffer:
             break
         byte += len(buffer)
-        word = len(list(filter(lambda s: len(s) > 0, buffer.split())))
+        word += len(list(filter(lambda s: len(s) > 0, buffer.split())))
         newline += 1
     return newline, word, byte
 
 
 @click.command()
-@click.argument('file', type=click.File('r'), nargs=-1)
+@click.argument('file', type=click.File('rb'), nargs=-1)
 def main(file):
     newline, word, byte = (0, 0, 0)
     results = []
@@ -38,9 +38,9 @@ def main(file):
         if cur_file.name == "-":
             length = 7
     mx = max(newline, word, byte)
-    length = max(1 if (mx == 0) else math.floor(math.log10(mx)) + 1, length)
-    for result in results:
-        click.echo(f"{result[0]:{length}d}{result[1]:{length}d}{result[2]:{length}d} total")
+    length = max(1 if (mx == 0) else math.floor(math.log10(mx)) + 2, length)
+    for result, cur_file in zip(results, file):
+        click.echo(f"{result[0]:{length}d}{result[1]:{length}d}{result[2]:{length}d} {cur_file.name}")
     if len(file) > 1:
         click.echo(f"{newline:{length}d}{word:{length}d}{byte:{length}d} total")
 

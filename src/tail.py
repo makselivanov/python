ee707fb5318d05e0a6140ba17747a6d6738da793
Makar Selivanov <makar.selivanov@gmail.com>
2024-02-27 03:06:05 +0300

    add tail

---
+++ src/tail.py
@@ -0,0 +1,33 @@
+import click
+
+TAIL_SIZE = 10
+
+
+def tail(file):
+    buffer = []
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        buffer.append(line)
+        if len(buffer) > TAIL_SIZE:
+            buffer.pop(0)
+    for line in buffer:
+        click.echo(line)
+
+
+@click.command()
+@click.argument('file', type=click.File('r'), nargs=-1)
+def main(file):
+    if len(file) == 1:
+        tail(file[0])
+    else:
+        for index, file in enumerate(file):
+            click.echo(f"==> {file} <==")
+            tail(file)
+            if index + 1 != len(file):
+                click.echo()
+
+
+if __name__ == '__main__':
+    main()
